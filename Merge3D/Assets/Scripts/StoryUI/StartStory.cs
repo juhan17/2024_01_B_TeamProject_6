c7b3f2a6c54411c2aa59efd3c7878163479f26f3
juhan17 <limjuhan17@gmail.com>
2024-06-12 12:53:11 +0900

    [Feat] 스토리씬 베이스

---
+++ Merge3D/Assets/Scripts/StoryUI/StartStory.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartStory : MonoBehaviour
+{
+    public Text textUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textUI.text = "Æë´«ÀÌ ¾îÂ¼°í ÀúÂ¼°í Çß´Âµ¥";
+    }
+}
