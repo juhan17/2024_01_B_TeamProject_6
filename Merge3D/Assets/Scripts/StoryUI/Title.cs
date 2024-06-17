cc01da270e496c1fc056c27614be9e45a9541f54
juhan17 <limjuhan17@gmail.com>
2024-06-17 22:14:27 +0900

    [Feat] 씬이동 추가 완료

---
+++ Merge3D/Assets/Scripts/StoryUI/Title.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Title : MonoBehaviour
+{
+    public Text textUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textUI.text = "펭눈의 모험";
+    }
+}
